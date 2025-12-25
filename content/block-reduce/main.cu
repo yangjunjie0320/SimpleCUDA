f68dadb81c30f67c67f7e5e19e272b10b4f8e077
Junjie Yang <yangjunjie0320@gmail.com>
2025-12-24 16:10:52 -0800

    update

--- content/block-reduce/main.cu
+++ content/block-reduce/main.cu
@@ -7,7 +7,6 @@
 #include "utils.cu"
 
 namespace block_reduce {
-#include "block-reduce-v0.cu"
 #include "block-reduce-v1.cu"
 #include "block-reduce-v2.cu"
 #include "block-reduce-v3.cu"
@@ -26,17 +25,12 @@ int main() {
         auto grid_dim = dim3(num_block_in_grid);
 
         std::string title =
-            std::format("block_reduce_f32_v0 (block_size = {:4})", num_thread_in_block);
-        auto config =
-            KernelLaunchConfig(block_reduce::kernel_v0, title.c_str(), block_dim, grid_dim);
-        auto result = config.run(inp);
-        result.print(true, true);
-
+            std::format("block_reduce_f32_v1 (block_size = {:4})", num_thread_in_block);
         int shared_mem_size = num_thread_in_block * sizeof(float);
         title = std::format("block_reduce_f32_v1 (block_size = {:4})", num_thread_in_block);
-        config = KernelLaunchConfig(block_reduce::kernel_v1, title.c_str(), block_dim, grid_dim,
+        auto config = KernelLaunchConfig(block_reduce::kernel_v1, title.c_str(), block_dim, grid_dim,
                                     shared_mem_size);
-        result = config.run(inp);
+        auto result = config.run(inp);
         result.print(false, true);
 
         title = std::format("block_reduce_f32_v2 (block_size = {:4})", num_thread_in_block);
@@ -55,11 +49,6 @@ int main() {
         num_warp_in_block = num_thread_in_block / NUM_THREAD_IN_WARP;
         block_dim = dim3(NUM_THREAD_IN_WARP, num_warp_in_block);
         grid_dim = dim3(num_block_in_grid);
-
-        title = std::format("block_reduce_f32_v0 (block_size = {:4})", num_thread_in_block);
-        config = KernelLaunchConfig(block_reduce::kernel_v0, title.c_str(), block_dim, grid_dim);
-        result = config.run(inp);
-        result.print(false, true);
 
         shared_mem_size = num_thread_in_block * sizeof(float);
         title = std::format("block_reduce_f32_v1 (block_size = {:4})", num_thread_in_block);
